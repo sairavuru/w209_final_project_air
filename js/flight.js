2c8994ab4e8d3c347f53555dc3cb9c02892652e5
Gary Pei <guangyu.pei@icloud.com>
2018-10-10 14:09:51 -0700

    Add functions to read in all data

--- js/flight.js
+++ js/flight.js
@@ -35,5 +35,69 @@ function sizeChange() {
 
 d3.select(window).on("resize", sizeChange);
 
+// get all data
+
+Promise.all([
+    d3.text("./Data/routes.csv"),
+    d3.text("./Data/airlines.csv"),
+    d3.text("./Data/airports.csv"),
+    d3.text("./Data/planes.csv")
+]).then(function(files) {
+    var routesData = d3.csvParseRows(files[0], function(d, i) {
+        return {
+        airline_code: d[0],
+        airline_ID: +d[1],
+        src_port_code: d[2],
+        src_port_id: +d[3],
+        dest_port_code: d[4],
+        dest_port_id: +d[5],
+        code_share: +d[6],
+        stops: +d[7],
+        equipment: d[8]};
+    });
+
+    var airlineData = d3.csvParseRows(files[1], function(d, i) {
+        return {
+        airline_ID: +d[0],
+        airline_name: d[1],
+        alias: d[2],
+        iata: d[3],
+        airline_code: d[4],
+        calsing: d[5],
+        country: d[6],
+        active: d[7]};
+    });
+
+    var airportData = d3.csvParseRows(files[2], function(d, i) {
+        return {
+        airport_ID: +d[0],
+        airport_name: d[1],
+        city_name: d[2],
+        country_name: d[3],
+        iata_code: d[4],
+        icao_code: d[5],
+        lat: +d[6],
+        long: +d[7],
+        altitude: +d[8],
+        tz_offset: +d[9],
+        DST: d[10],
+        tz_name: d[11],
+        airport_type: d[12],
+        source_data: d[13]};
+    });
+
+    var planesData = d3.csvParseRows(files[3], function(d, i) {
+        return {
+        aircraft_name: d[0],
+        iata_name: d[1],
+        icao_name: d[2]};
+    });
+
+    //console.log(planesData);
+
+}).catch(function(err) {
+    // handle error here
+})
+
 var routes = flight_viz_lib.routemapPlot();
 routes.plotworld();
